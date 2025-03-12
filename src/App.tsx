486192a5785a89875aeef4baeb74547db61a57be
jmau949 <44676775+jmau949@users.noreply.github.com>
2025-03-11 20:32:03 -0500

    undo lazy loading

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,4 @@
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-import { lazy, Suspense } from "react";
 import RootLayout from "./layouts/RootLayout";
 import PrivateRoute from "./components/PrivateRoute";
 import { AuthProvider } from "./auth/AuthProvider";
@@ -8,20 +7,15 @@ import { Provider } from "react-redux";
 import { store, persistor } from "./redux/store";
 import { PersistGate } from "redux-persist/integration/react";
 
-// Lazy-loaded components
-const HomePage = lazy(() => import("./pages/HomePage"));
-const LoginPage = lazy(() => import("./pages/auth/LoginPage"));
-const SignUpPage = lazy(() => import("./pages/auth/SignUpPage"));
-const ForgotPasswordPage = lazy(
-  () => import("./pages/auth/ForgotPasswordPage")
-);
-const ConfirmEmailPage = lazy(() => import("./pages/auth/ConfirmEmailPage"));
-const ResetPasswordPage = lazy(() => import("./pages/auth/ResetPasswordPage"));
-const NotFoundPage = lazy(() => import("./pages/errors/NotFoundPage"));
-const TestPage = lazy(() => import("./pages/auth/TestPage"));
-
-// Fallback UI while lazy-loaded components are loading
-const Loading = () => <div>Loading...</div>;
+// Direct imports instead of lazy loading
+import HomePage from "./pages/HomePage";
+import LoginPage from "./pages/auth/LoginPage";
+import SignUpPage from "./pages/auth/SignUpPage";
+import ForgotPasswordPage from "./pages/auth/ForgotPasswordPage";
+import ConfirmEmailPage from "./pages/auth/ConfirmEmailPage";
+import ResetPasswordPage from "./pages/auth/ResetPasswordPage";
+import NotFoundPage from "./pages/errors/NotFoundPage";
+import TestPage from "./pages/auth/TestPage";
 
 const App: React.FC = () => (
   <ErrorBoundary>
@@ -30,38 +24,33 @@ const App: React.FC = () => (
         <AuthProvider>
           <Router>
             <RootLayout>
-              <Suspense fallback={<Loading />}>
-                <Routes>
-                  <Route path="/login" element={<LoginPage />} />
-                  <Route path="/signup" element={<SignUpPage />} />
-                  <Route
-                    path="/forgot-password"
-                    element={<ForgotPasswordPage />}
-                  />
-                  <Route path="/confirm-email" element={<ConfirmEmailPage />} />
-                  <Route
-                    path="/reset-password"
-                    element={<ResetPasswordPage />}
-                  />
-                  <Route
-                    path="/"
-                    element={
-                      <PrivateRoute>
-                        <HomePage />
-                      </PrivateRoute>
-                    }
-                  />
-                  <Route
-                    path="/test"
-                    element={
-                      <PrivateRoute>
-                        <TestPage />
-                      </PrivateRoute>
-                    }
-                  />
-                  <Route path="*" element={<NotFoundPage />} />
-                </Routes>
-              </Suspense>
+              <Routes>
+                <Route path="/login" element={<LoginPage />} />
+                <Route path="/signup" element={<SignUpPage />} />
+                <Route
+                  path="/forgot-password"
+                  element={<ForgotPasswordPage />}
+                />
+                <Route path="/confirm-email" element={<ConfirmEmailPage />} />
+                <Route path="/reset-password" element={<ResetPasswordPage />} />
+                <Route
+                  path="/"
+                  element={
+                    <PrivateRoute>
+                      <HomePage />
+                    </PrivateRoute>
+                  }
+                />
+                <Route
+                  path="/test"
+                  element={
+                    <PrivateRoute>
+                      <TestPage />
+                    </PrivateRoute>
+                  }
+                />
+                <Route path="*" element={<NotFoundPage />} />
+              </Routes>
             </RootLayout>
           </Router>
         </AuthProvider>
